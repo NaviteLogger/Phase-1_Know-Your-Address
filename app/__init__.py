b6792140143cb73e308aa24889687ec955f7301b
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-03 01:28:28 +0100

    Added the explanation of the application factory

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,16 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+""" 
+The "Application Factory" (in this case the 'create_app' function) is a 
+pattern in Flask where the application is created inside a function,
+allowing for different instances of the app to be created with different 
+configurations. This is particularly useful for:
+- Configuring applications differently for different environments (development, testing, production, etc.)
+- Ensuring that resources (like databases) are correctly set up every time an app instance is created.
+- Facilitating unit testing by easily allowing the creation of a fresh app instance for each test.
+"""
+
 
 def create_app(test_config=None):
     # create and configure the app
