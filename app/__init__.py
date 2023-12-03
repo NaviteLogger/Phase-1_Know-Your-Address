03a4c0ae6186d8ed08362aaffe007af4fae0525d
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-03 01:31:08 +0100

    Added the import of the default production configuration to the application factory

--- app/__init__.py
+++ app/__init__.py
@@ -21,13 +21,14 @@ def create_app(test_config=None):
     # Create and configure the app
     app = Flask(__name__)
 
-    # Load the instance config, if it exists, when not testing
-    if test_config is None:
-        # Load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # Load the test config if passed in
-        app.config.from_mapping(test_config)
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Set the default config file
+    env = os.environ.get("FLASK_ENV", "production")  # Default to production
 
     # Ensure the instance folder exists
     try:
