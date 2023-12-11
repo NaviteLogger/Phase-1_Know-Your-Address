052a59acdc44eef55e7bf425e848a5b73180bda0
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-11 23:37:52 +0100

    Began adding the '/search' url route that would handle the searching and displaying the proper address area

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,7 +2,11 @@ from flask import render_template
 from . import main
 
 
-# Handle the GET request for the root url
+# Handle the GET request for the '/' url
 @main.route("/")
 def index():
     return render_template("index.html")
+
+
+# Handle the POST request for the '/search' url
+@main.route("/search", methods=["POST"])
