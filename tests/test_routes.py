c9fc1d78f66e67c361f8852f81dff6752ae54172
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-14 00:35:23 +0100

    Began creating unit test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,10 @@
 import pytest
 from flask import url_for
 from app import create_app
+
+
+@pytest.fixture
+def app():
+    app = create_app("config.TestingConfig")
+    with app.app_context():
+        yield app
