a388afd134e58516e6b2d162b0ae2db0c3fc2f65
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-14 20:28:34 +0100

    Began adding the test_search_route in 'test_routes.py' for testing purposes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,3 +8,8 @@ def app():
     app = create_app("config.TestingConfig")
     with app.app_context():
         yield app
+
+
+def test_search_route(client):
+    response = client.get(url_for("main.search"), data={"address": "Singapore"})
+    assert response.status_code == 200
