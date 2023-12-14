8f77fc606a96eb7503150081905a56129a98653c
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-14 21:13:44 +0100

    Trying to fix: ImportError: cannot import name 'models' from partially initialized module 'app.main' (most likely due to a circular import)

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -20,6 +20,3 @@ main = Blueprint("main", __name__)
 
 # Import the routes module from the 'routes.py' file located in the same directory as this __init__.py file.
 from . import routes
-
-# Import the models module from the 'models.py' file located in the same directory as this __init__.py file.
-from . import models
