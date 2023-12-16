aa6740d781c25a88c5cfea2c1715beae2a721c1e
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-16 22:22:06 +0100

    Removed the obtaining of OAuth functionality

--- app/main/obtainingOAuthToken.py
+++ app/main/obtainingOAuthToken.py
@@ -5,3 +5,18 @@
 from flask import current_app
 import requests
 
+
+def obtainOAuthToken():
+    # Get the Google Maps API key stored in the configurtion file
+    google_maps_api_key = current_app.config["GOOGLE_MAPS_API_KEY"]
+
+    # Build the URL for the Google Maps API
+    oauth_url = f"https://www.googleapis.com/oauth2/v4/token?client_id={google_maps_api_key}&client_secret={google_maps_api_key}&grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer"
+
+    # Send the request to the Google Maps API
+    oauth_response = requests.post(oauth_url).json()
+
+    # Extract the OAuth 2.0 access token from the response
+    oauth_token = oauth_response["access_token"]
+
+    return oauth_token
