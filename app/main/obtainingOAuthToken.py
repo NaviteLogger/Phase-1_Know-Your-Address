e7c4aca0144db101b1e89e17161e1c3f63bc4130
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-16 22:14:13 +0100

    Added the required imports to obtainingOAuthToke.py file

---
+++ app/main/obtainingOAuthToken.py
@@ -0,0 +1,7 @@
+# This file will containt the code for obtaining the OAuth 2.0 access token from the Google Maps API.
+# The following function will be exported to the routes.py file for obtaining the OAuth 2.0 access token:
+
+# Path: app/main/obtainingOAuthToken.py
+from flask import current_app
+import requests
+
