4b5b4eeade6d7b24f5280217a90cef667835eb51
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-16 22:08:48 +0100

    Reorganizing the main routing

--- app/main/routes.py
+++ app/main/routes.py
@@ -9,13 +9,13 @@ def index():
     return render_template("index.html")
 
 
-@main_bp.route("/retrieveCoordinatesForAnAddress", methods=["POST"])
-def retrieveCoordinatesForAnAddress():
+@main_bp.route("/retrieveCoordinatesForTheAddress", methods=["POST"])
+def retrieveCoordinatesForTheAddress():
     # Get the Google Maps API key stored in the configurtion file
     google_maps_api_key = current_app.config["GOOGLE_MAPS_API_KEY"]
 
     # Get the search address from the form
-    address = request.form["address"]
+    address = request.json["address"]
 
     # Step 1: Get the latitude and longitude of the address
     # Build the URL for the Google Maps API
@@ -23,48 +23,6 @@ def retrieveCoordinatesForAnAddress():
 
     # Send the request to the Google Maps API
     geocoding_response = requests.get(geocoding_url).json()
-
-    # Check the status code of the response
-    if geocoding_response["status"] == "OK":
-        # Get the latitude and longitude of the address
-        latitude = geocoding_response["results"][0]["geometry"]["location"]["lat"]
-        longitude = geocoding_response["results"][0]["geometry"]["location"]["lng"]
-
-        # Step 2: Using the latitude and longitude, get the nearby public transport stops
-        # Build the request for the Google Maps API
-        payload = {
-            "includedTypes": "bus_station|subway_station|train_station|transit_station",
-            "maxResultsCount": 10,
-            "locationRestriction": {
-                "circle": {"center": {"latitude": latitude, "longitude": longitude}, "radiusMeters": 1000}
-            },
-        }
-
-        # Define the headers for the request
-        headers = {
-            "Content-Type": "application/json",
-            "X-Goog-Api-Key": google_maps_api_key,
-            "X-Goog-FieldMask": "places.displayName",
-        }
-
-        # Make the POST request to the Google Maps API
-        url = "https://places.googleapis.com/v1/places:searchNearby"
-        response = requests.post(url, json=payload, headers=headers)
-
-        # Check the status code of the response
-        if response.status_code == 200:
-            # Process the response JSON as needed
-            result_data = response.json()
-            return jsonify(result_data)
-
-        else:
-            # Return an error message if the nearby stops are not found
-            error_message = f"Error {response.status_code}: {response.text}"
-            return jsonify({"error": error_message})
-
-    else:
-        # Return an error message if the address is not found
-        return jsonify({"error": "There was an error while fetching the address."}), 404
 
 
 @main_bp.route("/retrievePublicTransportInformationForTheGivenAddress", methods=["POST"])
