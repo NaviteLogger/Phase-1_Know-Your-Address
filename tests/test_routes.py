518909443cb8951875e0f6cb13c9d7180552e21e
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-17 23:14:25 +0100

    Refactor test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,16 +1,4 @@
 import pytest
-from app import create_app
-
-
-@pytest.fixture
-def app():
-    app = create_app("config.TestingConfig")
-    return app
-
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
 
 
 @pytest.mark.parametrize(
