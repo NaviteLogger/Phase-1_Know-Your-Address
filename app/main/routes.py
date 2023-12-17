ac84ab2265282f1c2711897bab74f0130aa05983
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-17 16:07:14 +0100

    Carried on the process of renaming to comply with the Python standards

--- app/main/routes.py
+++ app/main/routes.py
@@ -66,20 +66,13 @@ def manage_the_address_validation_result():
         # Return to the user the info about the address being invalid
         return jsonify({"addresValidationResult": "invalid"})
 
-    elif (
-        address_validation_result["result"]["verdict"]["addressComplete"] == True
-        and address_validation_result["result"]["verdict"]["validationGranularity"] != "OTHER"
-        and (address_validation_result["result"]["verdict"]["hasInferredComponents"] == True or address_validation_result["result"]["verdict"]["hasReplacesComponents"] == True)
-    ):
+    elif address_validation_result["result"]["verdict"]["addressComplete"] == True and address_validation_result["result"]["verdict"]["validationGranularity"] != "OTHER" and (address_validation_result["result"]["verdict"]["hasInferredComponents"] == True or address_validation_result["result"]["verdict"]["hasReplacesComponents"] == True):
         # The address is valid but requires the confirmations from the user - prompt the user to select the desired address from the list of suggestions
 
         # Return to the user the info about the address being valid but requiring the confirmation from the user
         return jsonify({"addressValidationResult": "validButFurtherUserInputIsRequired"})
 
-    elif (
-        address_validation_result["result"]["verdict"]["validationGranularity"] != "PREMISE" and
-        address_validation_result["result"]["verdict"]["validationGranularity"] != "SUB_PREMISE") or
-        address_validation_result["result"]["verdict"]["addressComplete"] == True:
+    elif (address_validation_result["result"]["verdict"]["validationGranularity"] != "PREMISE" or address_validation_result["result"]["verdict"]["validationGranularity"] != "SUB_PREMISE") and address_validation_result["result"]["verdict"]["addressComplete"] == True:
         # The address is valid and does not require any further input from the user
 
         # Return to the user the info about the address being valid
