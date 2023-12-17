f5b9c38578e3ea2805bf1c170cfacc027969f719
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-17 22:34:45 +0100

    Refactor API handler and remove unused file

--- app/main/api_handler.py
+++ app/main/api_handler.py
@@ -1,10 +1,5 @@
 from flask import jsonify
 import requests
 
-def send_api_request(address_validation_result):
-    # Check which json is incoming: the one from the client or the one from the Google Maps API
-    if "responseId" in address_validation_result:
-        # The incoming json is the one from the Google Maps API
-
-    else:
-        # The incoming json is the one from the client
+def send_request_to_valide_address():
+    
