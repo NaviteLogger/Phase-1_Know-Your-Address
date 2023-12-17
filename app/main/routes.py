024d76341f3ad094dad97fec586514e6c8cb650b
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-17 13:02:30 +0100

    Added the returing of the info about the address being invalid to the user

--- app/main/routes.py
+++ app/main/routes.py
@@ -66,6 +66,11 @@ def manageTheAddressValidationResult():
         # Store the unvalidated address in the session
         session["unvalidated_address"] = address_validation_result.get("address")
 
+        # Return to the user the info about the address being invalid
+        return jsonify({"addressValidationResult": "invalid"})
+    
+    
+
 
 @main_bp.route("/retrieveCoordinatesForTheAddress", methods=["POST"])
 def retrieveCoordinatesForTheAddress():
