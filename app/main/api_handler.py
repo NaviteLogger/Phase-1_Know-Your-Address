6498c640fec328f9c037329787a14f7f7dae28a5
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-17 22:54:43 +0100

    Add request payload for Google Maps API

--- app/main/api_handler.py
+++ app/main/api_handler.py
@@ -5,3 +5,10 @@ import requests
 def send_request_to_valide_address(address):
     # Get the Google Maps API key stored in the configurtion file
     google_maps_api_key = current_app.config["GOOGLE_MAPS_API_KEY"]
+
+    # Build the request for the Google Maps API
+    payload = {
+        "address": {
+            "addressLine1": address,
+        }
+    }
