44abea164d62e122f0f2e0faf10c5dfc88e43e5d
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-17 14:00:59 +0100

    Added the returing of the info about the address being sufficiently good but requiring confirmation

--- app/main/routes.py
+++ app/main/routes.py
@@ -66,12 +66,16 @@ def manageTheAddressValidationResult():
 
         # Return to the user the info about the address being invalid
         return jsonify({"addressValidationResult": "invalid"})
-    
-    elif (address_validation_result["result"]["verdict"]["addressComplete"] == True and 
-        address_validation_result["result"]["verdict"]["validationGranularity"] != "OTHER" and 
-        (address_validation_result["result"]["verdict"]["hasInferredComponents"] == True or
-        address_validation_result["result"]["verdict"]["hasReplacesComponents"] == True)):
-        
+
+    elif (
+        address_validation_result["result"]["verdict"]["addressComplete"] == True
+        and address_validation_result["result"]["verdict"]["validationGranularity"] != "OTHER"
+        and (address_validation_result["result"]["verdict"]["hasInferredComponents"] == True or address_validation_result["result"]["verdict"]["hasReplacesComponents"] == True)
+    ):
+        # The address is valid but requires the confirmations from the user - prompt the user to select the desired address from the list of suggestions
+
+        # Return to the user the info about the address being valid but requiring the confirmation from the user
+        return jsonify({"addressValidationResult": "validButRequiresFurtherInput"})
 
 
 @main_bp.route("/retrieveCoordinatesForTheAddress", methods=["POST"])
