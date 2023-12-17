8d47ab7a5e67ecd2c5d57d305ff3d199ae95f4ed
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-17 23:23:07 +0100

    Refactor test_save_the_given_address_to_session function to use JSON payload

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -9,5 +9,9 @@ import pytest
     ],
 )
 def test_save_the_given_address_to_session(client, input_address, expected_status):
-    response = client.post("/save-the-given-address-to-session", data={"address": input_address})
-    assert response.expected_status == "success"
+    response = client.post("/save-the-given-address-to-session", json={"address": input_address})
+
+    assert response.status_code == 200  # Check the HTTP status code
+
+    data = response.json
+    assert data["status"] == expected_status
