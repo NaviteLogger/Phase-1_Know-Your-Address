7b469e45aeb8dd3b0824294a463aa812508e2afd
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-17 22:52:55 +0100

    Add Google Maps API key retrieval from configuration file

--- app/main/api_handler.py
+++ app/main/api_handler.py
@@ -1,4 +1,7 @@
-from flask import jsonify
+from flask import jsonify, current_app
 import requests
 
+
 def send_request_to_valide_address(address):
+    # Get the Google Maps API key stored in the configurtion file
+    google_maps_api_key = current_app.config["GOOGLE_MAPS_API_KEY"]
