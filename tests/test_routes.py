fd2f44fc2a37a07d0ac242d0e25be75ad806ccb0
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-17 22:38:12 +0100

    Add test cases for saving addresses to session

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -9,6 +9,13 @@ def app():
         yield client
 
 
+@pytest.mark.parametrize(
+    "input_address, expected_status_code",
+    [
+        ("", 400),
+        ("Aleja KEN 115", 200),
+    ],
+)
 @pytest.mark.parametrize()
 def test_save_the_given_address_to_session(client, input_address, expected_status_code):
     response = client.post("/save-the-given-address-to-session", data={"address": input_address})
