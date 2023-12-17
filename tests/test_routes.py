1944c51c3dd03f045c887b058b047d105f91f940
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-17 23:18:58 +0100

    Refactor test_routes.py to use descriptive status strings

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,12 +2,12 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "input_address, expected_status_code",
+    "input_address, expected_status",
     [
-        ("", 400),
-        ("Aleja KEN 115", 200),
+        ("Mazowiecka 15/3", "success"),
+        ("Aleja KEN 115", "success"),
     ],
 )
-def test_save_the_given_address_to_session(client, input_address, expected_status_code):
+def test_save_the_given_address_to_session(client, input_address, expected_status):
     response = client.post("/save-the-given-address-to-session", data={"address": input_address})
-    assert response.status_code == expected_status_code
+    assert response.status == "success"
