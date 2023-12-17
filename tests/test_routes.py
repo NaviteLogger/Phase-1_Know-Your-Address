dcd808dd1115b2633339b0d4f9e944016cc6d2ae
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-17 23:06:13 +0100

    Remove unused pytest.mark.parametrize() decorator

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -16,7 +16,6 @@ def app():
         ("Aleja KEN 115", 200),
     ],
 )
-@pytest.mark.parametrize()
 def test_save_the_given_address_to_session(client, input_address, expected_status_code):
     response = client.post("/save-the-given-address-to-session", data={"address": input_address})
     assert response.status_code == expected_status_code
