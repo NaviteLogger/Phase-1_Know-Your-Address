17ac270391b49384610e1789322ba459a1720077
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-17 23:14:08 +0100

    Update function to handle edge cases
    This commit updates the function to handle edge cases, improving the overall reliability and robustness of the code.

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from app import create_app
+
+
+@pytest.fixture
+def app():
+    app = create_app(test_config={"TESTING": True, "WTF_CSRF_ENABLED": False})
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
