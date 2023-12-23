81b6c4e2ab8e51bdd1a2bea8f81b264dd218f355
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-23 15:52:23 +0100

    Add alternative method to check if address is URL encoded

--- app/main/additional_functions.py
+++ app/main/additional_functions.py
@@ -9,3 +9,16 @@ def is_url_encoded(address):
         return True
     else:
         return False
+
+
+"""
+There is another way to check whether the address is already URL encoded:
+def is_url_encoded(s):
+    try:
+        decoded = urllib.parse.unquote(s)
+        encoded = urllib.parse.quote(decoded)
+        return encoded == s
+    except Exception as e:
+        # An exception might occur if the input is not a valid URL-encoded string
+        return False
+"""
