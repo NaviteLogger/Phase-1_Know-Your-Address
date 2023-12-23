56c1ef3df3ba84628208e630515b2c37fba4df99
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-23 15:51:54 +0100

    Add URL encoding check in is_url_encoded function

--- app/main/additional_functions.py
+++ app/main/additional_functions.py
@@ -5,4 +5,7 @@ def is_url_encoded(address):
     decoded = urllib.parse.unquote(address)
     encoded = urllib.parse.quote(decoded)
 
-    
+    if encoded == address:
+        return True
+    else:
+        return False
