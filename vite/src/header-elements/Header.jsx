51f8cd3eb8cd9663a279cd86c6275d497d824f37
Marek Kacprzak <kacprzakmarek92@gmail.com>
2024-01-03 21:04:45 +0100

    Updated the react elements

---
+++ vite/src/header-elements/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css';
+import Text from './Text.jsx';
+
+function Header() {
+  return (
+    <div className='main-header-div'>
+      <header>
+        <Text />
+      </header>
+    </div>
+  )
+}
+
+export default Header;
