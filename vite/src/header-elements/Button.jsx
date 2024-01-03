3d8e8a6df5463867c9836772191d9deeb65b2fc1
Marek Kacprzak <kacprzakmarek92@gmail.com>
2024-01-03 21:12:58 +0100

    Added new 'Button.jsx' file

---
+++ vite/src/header-elements/Button.jsx
@@ -0,0 +1,9 @@
+function Button() {
+    return (
+        <div>
+            <button>Click Me</button>
+        </div>
+    )
+}
+
+export default Button;
--- vite/src/header-elements/Header.jsx
+++ vite/src/header-elements/Header.jsx
@@ -1,11 +1,13 @@
 import './Header.css';
 import Text from './Text.jsx';
+import Button from './Button.jsx';
 
 function Header() {
   return (
     <div className='main-header-div'>
       <header>
         <Text />
+        <Button />
       </header>
     </div>
   )
